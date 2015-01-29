64fd43c74c66bf1b9ca82100c3d51fb3c6ab1e88
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-01-30 00:28:53 +0530

    Create categories model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119163724) do
+ActiveRecord::Schema.define(version: 20150129175151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20150119163724) do
     t.text     "title"
     t.text     "url"
     t.integer  "votes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
