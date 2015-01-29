920a708475e09218a4c47c6a9bb09223c3860a44
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-01-29 22:10:59 +0530

    Show the latest first on articles page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = Article.paginate({ page: params[:page], per_page: 15 })
+    @articles = Article.paginate({ page: params[:page], per_page: 15 }).order('created_at DESC')
   end
 
   def new
