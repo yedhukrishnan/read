59d110f4f24c2b00c33684ee03d1c9cbff84fde3
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-01-30 19:11:05 +0530

    Fix a failing test

--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "articles/index.html.erb", :type => :view do
 
   before(:example) do
-    Article.create({ title: "Sample Article", url: "http://sample1.com" })
-    Article.create({ title: "Lorem Ipsum", url: "https://sample2.com" })
+    category = Category.create({ name: "dummy" })
+    Article.create({ title: "Sample Article", url: "http://sample1.com", category_id: category.id })
+    Article.create({ title: "Lorem Ipsum", url: "https://sample2.com", category_id: category.id })
     assign(:articles, Article.paginate({ page: 1, per_page: 15 }))
   end
   
