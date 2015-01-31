3f291bde28d011a3697741cc4d9580b43d4fa381
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-01-31 16:16:34 +0530

    Refactor specs

--- spec/views/articles/new.html.erb_spec.rb
+++ spec/views/articles/new.html.erb_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe "articles/new.html.erb", :type => :view do
 
   it "enables us to post a new article" do
     assign(:article, Article.new)
-    Category.create({ name: "MyCategory" })
+    category = Category.create({ name: "MyCategory" })
     visit "/articles/new"
     fill_in "Title", with: "article title"
     fill_in "Url", with: "http://google.com"
-    select "MyCategory", from: "Category"
+    select category.name, from: "Category"
     expect(lambda{ click_button "Post Article" }).to change{ Article.count }.by(1)
   end
   
