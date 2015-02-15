6f1e562be879c1107f7fba2b3ea426a0752de015
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-02-15 09:30:41 +0530

    Revert: Add username to devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
-  attr_accessor :login
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
-
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    else
-      if conditions[:username].nil?
-        where(conditions).first
-      else
-        where(username: conditions[:username]).first
-      end
-    end
-  end
+    :recoverable, :rememberable, :trackable, :validatable
 
 end
