9bfb13a4d649c7c1b12223920fe3b352d874226f
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-03-19 11:37:04 +0530

    Modify upvote to add validations

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,4 +1,6 @@
 class Upvote < ActiveRecord::Base
+  validates :user_id, presence: true
+  validates :article_id, presence: true
   belongs_to :user
   belongs_to :article
 end
--- spec/models/upvote_spec.rb
+++ spec/models/upvote_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Upvote, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject{ Upvote.new }
+  
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :article_id }
+  
 end
